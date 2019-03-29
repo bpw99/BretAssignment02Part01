4ed4bf55987d96137851f40ddf09c8f9fbeef60a
Lex von Biedenfeld <lexvonbon@users.noreply.github.com>
2019-03-29 00:30:25 -0500

    Candy Fix

--- src/Candy.java
+++ src/Candy.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 public class Candy implements ICandyVendingMachine, Cloneable {
 	
 	String name;
@@ -64,70 +63,3 @@ public class Candy implements ICandyVendingMachine, Cloneable {
 	}
 
 }
-=======
-public class Candy implements ICandyVendingMachine, Cloneable {
-	
-	String name;
-	double price;
-	public double getPrice() {
-		return price;
-	}
-
-	public void setPrice(double price) {
-		this.price = price;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	
-	// Constructor
-	public Candy() {}
-	
-	public Candy(String n, double p) {
-		price = p;
-		name = n;
-	}
-
-	@Override
-	public void TakeMoney(double amount) 
-	{
-		
-	}
-
-	@Override
-	public void ReturnMoney(double amount) 
-	{
-		
-	}
-
-	@Override
-	public Object VendItem(String slotCode) {
-		
-		return null;
-	}
-
-	@Override
-	public String GetMachineInfo() {
-		
-		return null;
-	}
-
-	@Override
-	public String DisplayContents() {
-		
-		return null;
-	}
-	
-	public Candy clone()
-	{
-		return new Candy(this.name,this.price);
-	}
-
-}
->>>>>>> refs/remotes/origin/master
